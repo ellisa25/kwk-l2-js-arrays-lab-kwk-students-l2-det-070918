7bed757b632acaa71f5244db3f01cb34e7b1f3c4
ellisa25 <ewhill0306@gmail.com>
2018-07-11 20:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver (name) {drivers.push(name);}
 function destructivelyPrependDriver (name) {drivers.unshift(name);}
 function destructivelyRemoveLastDriver (name) {drivers.pop(name);}
-function 
+function destructivelyRemoveFirstDriver (name) {drivers.shift
